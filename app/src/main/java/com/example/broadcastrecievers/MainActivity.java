4653f242633ae62a06f2ce37de741d73b54d6022
roeys <roeys1234@gmail.com>
2024-01-22 11:22:39 +0200

    added restart broadcast receiver

--- app/src/main/java/com/example/broadcastrecievers/MainActivity.java
+++ app/src/main/java/com/example/broadcastrecievers/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.broadcastrecievers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
         TextView power, headphones, times_headphones;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "ApplySharedPref"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
         power = findViewById(R.id.power_view);
         headphones = findViewById(R.id.headphones_view);
         times_headphones = findViewById(R.id.times_headphones_view);
-        
+
+        SharedPreferences settings = getSharedPreferences("boot_counter", MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("counter", 0);
+        editor.commit();
+
+        String power_str = power.getText().toString();
+        power_str.replace('0', (char) (settings.getInt("counter", 0)));
+        power.setText(power_str);
     }
 }
---
+++ app/src/main/java/com/example/broadcastrecievers/broadcasts/RestartReceiver.java
@@ -0,0 +1,21 @@
+package com.example.broadcastrecievers.broadcasts;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.TextView;
+
+public class RestartReceiver extends BroadcastReceiver {
+
+    @SuppressLint("ApplySharedPref")
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences settings = context.getSharedPreferences("boot_counter", Context.MODE_PRIVATE);
+        int counter = settings.getInt("counter", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("counter", counter + 1);
+        editor.commit();
+    }
+}
